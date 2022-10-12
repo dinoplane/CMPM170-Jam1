854f5567208fc6238df6c63593bdd5cca2879f29
JodagonDrago <josephHaloman0@gmail.com>
2022-10-11 22:28:25 -0700

    Revert "Merge branch 'main' of https://github.com/dinoplane/CMPM170-Jam1"
    This reverts commit 9fa10f9f3ede4a4549b643c12e5f6c353698c062, reversing
    changes made to a15a0781ecfa45f220cad7c3bde5934021dc9e62.

--- Assets/Scripts/MusicSource/MusicSourceBehavior.cs
+++ Assets/Scripts/MusicSource/MusicSourceBehavior.cs
@@ -6,15 +6,10 @@ public class MusicSourceBehavior : MonoBehaviour
 {
     private GameObject currGremlin;
     private bool isSpawned = false;
-
-    // public GameObject musicBox = null;
-    // public static short musicCount = 0;
-    public FinaleManagerScript finale;
-
     // Start is called before the first frame update
     void Start()
     {
-        // musicBox = GameObject.Find("FinalSong");
+        
     }
 
     void OnTriggerEnter(Collider collision)
@@ -25,10 +20,6 @@ public class MusicSourceBehavior : MonoBehaviour
             currGremlin = collision.gameObject;
             isSpawned = true;
             gameObject.GetComponent<FMODUnity.StudioEventEmitter>().SetParameter("IsGremlinAlive", 1);
-            // musicBox = GameObject.Find("FinalSong");
-            // musicBox.SetActive(true);
-            // Instantiate(musicBox, transform.position, Quaternion.identity);
-            finale.musicCount++;
         }
     }
 
@@ -46,16 +37,10 @@ public class MusicSourceBehavior : MonoBehaviour
     void Update()
     {   
         // Check if gremlin is destroyed
-        if (isSpawned && currGremlin == null && finale.musicCount < 5)   {
+        if (isSpawned && currGremlin == null)   {
            gameObject.GetComponent<FMODUnity.StudioEventEmitter>().SetParameter("IsGremlinAlive", 0);
             isSpawned = false;
-            finale.musicCount--;
         }
-        else if (finale.musicCount == 5)
-        {
-            isSpawned = false;
-            gameObject.GetComponent<FMODUnity.StudioEventEmitter>().SetParameter("IsGremlinAlive", 0);
-            // input invincible gremlin logic here;
-        }
+     
     }
 }
