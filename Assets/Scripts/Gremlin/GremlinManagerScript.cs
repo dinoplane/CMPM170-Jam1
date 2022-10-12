25cece28046a445ab1402363cb81e1eeb9e47534
dinoplane <isacsac2017@gmail.com>
2022-10-11 23:02:54 -0700

    Revert "Revert "Merge branch 'main' into EmersensBranch""
    This reverts commit 9e61a07b8de99c1f2619f56a0516877d2347673b.

--- Assets/Scripts/Gremlin/GremlinManagerScript.cs
+++ Assets/Scripts/Gremlin/GremlinManagerScript.cs
@@ -7,6 +7,7 @@ public class GremlinManagerScript : MonoBehaviour
     
     public float RER = 3.0f; //Random Encounter Rate
     public float elapsedTime;
+    public float totalTime;
     //RER varies based on number of gremlins spawned
     //every time we add a gremlin, check if we have reached max, if so start climax
     public GameObject GremlinPrefab;
@@ -21,6 +22,7 @@ public class GremlinManagerScript : MonoBehaviour
             freeSpawns.Add(x);
         }
         elapsedTime = 0;
+        totalTime = 0;
     }
 
     // Update is called once per frame
@@ -28,6 +30,7 @@ public class GremlinManagerScript : MonoBehaviour
     {
         //increment timer, if timer==RER
         elapsedTime += Time.deltaTime;
+        totalTime += Time.deltaTime;
         if (elapsedTime >= RER)
         {
             // Spawn Gremlin
@@ -41,11 +44,19 @@ public class GremlinManagerScript : MonoBehaviour
 
     void IncrementRER()
     {
+        if (freeSpawns.Count == Spawnpoints.Length-1)
+        {
+            RER += 2;
+        }
         RER += 1;
     }
 
     void DecrementRER()
     {
+        if (freeSpawns.Count == Spawnpoints.Length)
+        {
+            RER = 2;
+        }
         RER -= 1;
     }
 
@@ -59,7 +70,8 @@ public class GremlinManagerScript : MonoBehaviour
 
         // Get random location of spawnpoint to place gremlin
         int loc = Random.Range(1, freeSpawns.Count)-1;
-        Vector3 spawnPos= Spawnpoints[freeSpawns[loc]].transform.position; // get position in 3D coordinates of spawnpoint object
+        //int loc2 = Random.Range(1, Spawnpoints[loc].Length) - 1;
+        Vector3 spawnPos= Spawnpoints[freeSpawns[loc]]/*[loc2]*/.transform.position; // get position in 3D coordinates of spawnpoint object
         
         GameObject babyGremlin = Instantiate(GremlinPrefab, spawnPos, Quaternion.identity); // Create gremlin instance and place it
         babyGremlin.GetComponent<GremlinScript>().setSpawnNum(freeSpawns[loc]);
@@ -74,5 +86,6 @@ public class GremlinManagerScript : MonoBehaviour
     {
         freeSpawns.Add(num);
         DecrementRER();
+        
     }
 }
